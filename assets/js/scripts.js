2c3f22576ac5c19db5a2632eedceef8b9033f443
Anvar <Anvar-abd_97@mail.ru>
2020-02-09 02:06:07 +0500

    done

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,4 +2,11 @@ jQuery(document).ready(function($) {
 	$(".clickable-row").click(function() {
 		window.location = $(this).data("href");
 	});
+
+	$("#sortTable").DataTable();
+	$("#sortTableModal").DataTable({
+		paging: false,
+		searching: false
+	});
+	$(".dataTables_length").addClass("bs-select");
 });
