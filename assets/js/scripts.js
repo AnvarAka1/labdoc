ac7afa761b8a4f0bbbcf641a32afd2c0a5ad8882
Anvar <Anvar-abd_97@mail.ru>
2020-02-09 17:49:25 +0500

    moment.js and some more fixes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,25 @@
 jQuery(document).ready(function($) {
+	moment.locale("ru");
 	$(".clickable-row").click(function() {
 		window.location = $(this).data("href");
 	});
-
 	$("#sortTable").DataTable();
 	$("#sortTableModal").DataTable({
 		paging: false,
 		searching: false
 	});
 	$(".dataTables_length").addClass("bs-select");
+	myCustomTime();
+	setInterval(() => {
+		myCustomTime();
+	}, 1000);
 });
+
+// const myCustomTime = () => {
+// 	$(".custom-time").html(moment().format("LT"));
+// 	$(".custom-date").html(moment().format("LL"));
+// };
+function myCustomTime() {
+	$(".custom-time").html(moment().format("LT"));
+	$(".custom-date").html(moment().format("LL"));
+}
